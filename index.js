c27ceb25b8750b2e7c24aa56b846c4f118d089e5
Ivana <ivana.nikolic121@gmail.com>
2019-01-30 02:05:51 +0100

    Sum function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('Hello world');
+var x;
+function sum(a,b){
+	 x = a+b;
+	 console.log(x);
+	
+}
+
+sum(2,3);
+sum(5,6);
+sum(1,2,3);
+sum(2);
+sum('i','v');
+sum(2,'3');
+sum();
+sum(5,'');
+sum(1,true);
+
