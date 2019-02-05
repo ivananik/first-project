ecb9ac631f13418df73a37037f35113570eb3d9f
Ivana <ivana.nikolic121@gmail.com>
2019-02-05 14:27:22 +0100

    Sum function update

--- index.js
+++ index.js
@@ -1,18 +1,89 @@
-console.log('Hello world');
-var x;
 function sum(a,b){
-	 x = a+b;
-	 console.log(x);
+	
+	if(a==='' || b === ''){
+		throw "empty";
+	}
+
+	else if(a=== true || b===true){
+		throw "boolean, not a number";
+	}
+
+	else if(a=== false || b=== false){
+		throw "boolean, not a number";
+	}
+
+	else if(a===undefined || b===undefined)
+		throw "no arguments";
+
+	if(a.length >0 || b.length >0){
+		throw "string, not a number";
+	}
+
+	else if(arguments.length >2){
+		throw "more than 2  arguments"
+	} 
+
+	
+
+	else {
+		return a + b;
+	}
 	
 }
 
-sum(2,3);
-sum(5,6);
-sum(1,2,3);
-sum(2);
-sum('i','v');
-sum(2,'3');
-sum();
-sum(5,'');
-sum(1,true);
+try{
+	console.log(sum('2','3'));
+}
+catch(error){
+	console.log(error)
+}
 
+
+try{
+	console.log(sum(true,1));
+}
+catch(error){
+	console.log(error)
+}
+
+try{
+	console.log(sum('',6));
+}
+catch(error){
+	console.log(error)
+}
+
+try{
+	console.log(sum(1,2,3));
+}
+catch(error){
+	console.log(error)
+}
+
+try{
+	console.log(sum());
+}
+catch(error){
+	console.log(error)
+}
+
+try{
+	console.log(sum(5,6));
+}
+catch(error){
+	console.log(error)
+}
+
+try{
+	console.log(sum('i','v'));
+}
+catch(error){
+	console.log(error)
+}
+
+try{
+	console.log(sum('',5));
+}
+catch(error){
+	console.log(error)
+}
